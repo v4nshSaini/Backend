d4641b649ace551579c9356b9c0b76e4df208662
Vansh <vansh@test.com>
2026-04-04 10:31:28 +0530

    Updated code with new changes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://hr-frontendd.vercel.app',
+    ],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
-    .setTitle('HR Management API')
-    .setDescription('API documentation')
+    .setTitle('HR API')
+    .setDescription('HR Management System API')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
@@ -18,6 +25,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
